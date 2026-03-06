bc456be2f246f52e32036155eb308bc44ead1203
AID133 <aid133@AID133s-MacBook-Air.local>
2026-03-06 12:57:23 +0800

    initial commit

---
+++ src/main/java/server/ChatServer.java
@@ -0,0 +1,13 @@
+package server;
+
+public class ChatServer {
+
+    public static void main(String[] args) {
+        System.out.println("test working") ;
+
+        // starts a session manager
+        //
+    }
+
+
+}
