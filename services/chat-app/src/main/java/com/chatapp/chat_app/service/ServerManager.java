f184edd28a4fe114c5e7213ff8d8359f918d46f4
zackr493 <zhanrong@zohocorp.com>
2026-03-10 09:42:40 +0800

    updated servermanager for db changes

--- services/chat-app/src/main/java/com/chatapp/chat_app/service/ServerManager.java
+++ services/chat-app/src/main/java/com/chatapp/chat_app/service/ServerManager.java
@@ -1,15 +1,14 @@
 package com.chatapp.chat_app.service;
 
-import com.chatapp.chat_app.dto.Client;
+import com.chatapp.chat_app.model.ClientEntity;
 import com.chatapp.chat_app.dto.ClientStatus;
-import com.chatapp.chat_app.dto.Server;
-import com.chatapp.chat_app.dto.Session;
+import com.chatapp.chat_app.model.ServerEntity;
+import com.chatapp.chat_app.model.SessionEntity;
 
 import java.time.LocalDateTime;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Semaphore;
-import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class ServerManager {
@@ -17,7 +16,7 @@ public class ServerManager {
 
     // normal arraylist is not thread safe, adding multiple threads will cause race conditions
     // synclist only one thread can access methods
-    private final List<Server> servers = Collections.synchronizedList(new ArrayList<Server>());
+    private final List<ServerEntity> serverEntities = Collections.synchronizedList(new ArrayList<ServerEntity>());
 
     // instead of queue, we use semaphore to allows access to a limited number of resource,
     // we dont need to handle locking, and we dont need strict FIFO waiting
@@ -32,8 +31,8 @@ public class ServerManager {
     private final AtomicInteger totalClients = new AtomicInteger() ;
     private final AtomicInteger lostClients = new AtomicInteger() ;
 
-    private final Map<String, Client> clientMap = new ConcurrentHashMap<>();
-    private final Map<String, Session> sessionMap = new ConcurrentHashMap<>();
+    private final Map<String, ClientEntity> clientMap = new ConcurrentHashMap<>();
+    private final Map<String, SessionEntity> sessionMap = new ConcurrentHashMap<>();
 
     public ServerManager(int maxServers) {
 
@@ -42,44 +41,44 @@ public class ServerManager {
 
         for (int i = 1; i <= maxServers; i++) {
             // arbituary naming
-            servers.add(new Server("Server" + i));
+            serverEntities.add(new ServerEntity("Server" + i));
 
         }
 
     }
 
     // CRUD for clients, in this case we do not need DELETE or UPDATE, we only need to update status
-    public boolean addClient(Client client) {
+    public boolean addClient(ClientEntity clientEntity) {
 
         System.out.println(clientMap);
-        System.out.println(client);
-        Client existing = clientMap.putIfAbsent(client.getId(), client);
+        System.out.println(clientEntity);
+        ClientEntity existing = clientMap.putIfAbsent(clientEntity.getId(), clientEntity);
 
         if (existing == null) {
-            client.setStatus(ClientStatus.CREATED);
+            clientEntity.setStatus(ClientStatus.CREATED);
 
             // uuid for session
             String sessionId = UUID.randomUUID().toString();
-            Session session = new Session(client, null);
+            SessionEntity sessionEntity = new SessionEntity(clientEntity, null);
 
-            sessionMap.put(sessionId, session) ;
+            sessionMap.put(sessionId, sessionEntity) ;
 
             return true;
         }
         return false;
     }
 
-    public Client getClient(String name) {
+    public ClientEntity getClient(String name) {
         return clientMap.get(name);
     }
 
 
-    public List<Client> getAllClients() {
+    public List<ClientEntity> getAllClients() {
         return new ArrayList<>(clientMap.values());
     }
 
-    public boolean updateClientStatus(Client client, ClientStatus status) {
-        Client existing = clientMap.get(client.getId());
+    public boolean updateClientStatus(ClientEntity clientEntity, ClientStatus status) {
+        ClientEntity existing = clientMap.get(clientEntity.getId());
 
         if (existing == null) {
             return false; // client does not exist
@@ -89,7 +88,7 @@ public class ServerManager {
         return true;
     }
 
-    public void handleClientJoining(Client client, int timeToExpiry) {
+    public void handleClientJoining(ClientEntity clientEntity, int timeToExpiry) {
 
         // thread safe increment
         totalClients.incrementAndGet();
@@ -101,33 +100,33 @@ public class ServerManager {
             // if server not acquired within 5minutes, we add to lost clients
             if (!serverAcquired) {
                 lostClients.incrementAndGet();
-                System.out.println("LOST CLIENT: " + client.getId());
+                System.out.println("LOST CLIENT: " + clientEntity.getId());
                 return;
 
 
             }
 
             // when server acquired, find server that is free, there should be one available
-            Server server = getAvailServer();
+            ServerEntity serverEntity = getAvailServer();
 
-            if (server == null) {
+            if (serverEntity == null) {
                 semaphore.release();
                 return;
             }
 
-            synchronized (server) {
-                server.setCurrClient(client);
+            synchronized (serverEntity) {
+                serverEntity.setCurrClientEntity(clientEntity.getId());
             }
 
 //          // attach server to existing session
             // find session tied to client
-            Session session = sessionMap.values().stream().filter(s -> s.getClient() == client).findFirst().orElse(null);
+            SessionEntity sessionEntity = sessionMap.values().stream().filter(s -> s.getClientEntity() == clientEntity).findFirst().orElse(null);
 
-            if (session != null) {
-                session.setServer(server);
+            if (sessionEntity != null) {
+                sessionEntity.setServerEntity(serverEntity);
             }
 
-            System.out.println(client.getId() + " is connected to " + server.getId());
+            System.out.println(clientEntity.getId() + " is connected to " + serverEntity.getId());
 
 
 
@@ -138,29 +137,29 @@ public class ServerManager {
     }
 
     // Return all servers
-    public List<Server> getServers() {
-        synchronized (servers) { // synchronized since servers list is wrapped in synchronizedList
-            return new ArrayList<>(servers);
+    public List<ServerEntity> getServers() {
+        synchronized (serverEntities) { // synchronized since servers list is wrapped in synchronizedList
+            return new ArrayList<>(serverEntities);
         }
     }
 
     // Get server by name
-    public Server getServerByName(String name) {
-        synchronized (servers) {
-            for (Server server : servers) {
-                if (server.getId().equals(name)) {
-                    return server;
+    public ServerEntity getServerByName(String name) {
+        synchronized (serverEntities) {
+            for (ServerEntity serverEntity : serverEntities) {
+                if (serverEntity.getId().equals(name)) {
+                    return serverEntity;
                 }
             }
         }
         return null;
     }
 
-    public Server getAvailServer() {
-        synchronized (servers) {
-            for (Server server : servers) {
-                if (server.getCurrClient() == null) {
-                    return server;
+    public ServerEntity getAvailServer() {
+        synchronized (serverEntities) {
+            for (ServerEntity serverEntity : serverEntities) {
+                if (serverEntity.getCurrClientEntity() == null) {
+                    return serverEntity;
                 }
             }
         }
@@ -170,30 +169,30 @@ public class ServerManager {
 
     public void finishSession(String sessionId, int rating) {
 
-        Session session = sessionMap.get(sessionId);
+        SessionEntity sessionEntity = sessionMap.get(sessionId);
 
-        if (session == null) {
+        if (sessionEntity == null) {
             System.out.println("Session not found");
             return;
         }
 
 
-        Server server = session.getServer();
-        Client client = session.getClient();
+        ServerEntity serverEntity = sessionEntity.getServerEntity();
+        ClientEntity clientEntity = sessionEntity.getClientEntity();
 
 
         // synchronized ensures only one thread at a time can enter a synchronized method on the same object
         // otheres will be blocked until lock released
-        if (server != null) {
-            synchronized (server) {
-                server.setNumClientsDay(server.getNumClientsDay() + 1);
-                server.setNumClientsMonth(server.getNumClientsMonth() + 1);
-                server.setRatingTotal(server.getRatingTotal() + rating);
-                server.setRatingCount(server.getRatingCount() + 1);
+        if (serverEntity != null) {
+            synchronized (serverEntity) {
+                serverEntity.setNumClientsDay(serverEntity.getNumClientsDay() + 1);
+                serverEntity.setNumClientsMonth(serverEntity.getNumClientsMonth() + 1);
+                serverEntity.setRatingTotal(serverEntity.getRatingTotal() + rating);
+                serverEntity.setRatingCount(serverEntity.getRatingCount() + 1);
 
-                System.out.println("CHAT ENDED | CLIENT: " + client.getId() + " SERVER: " + server.getId());
+                System.out.println("CHAT ENDED | CLIENT: " + clientEntity.getId() + " SERVER: " + serverEntity.getId());
 
-                server.setCurrClient(null);
+                serverEntity.setCurrClientEntity(null);
 
                 semaphore.release();
             }
@@ -203,8 +202,8 @@ public class ServerManager {
             System.out.println("No server assigned for session of client");
         }
 
-        session.setEndTime(LocalDateTime.now());
-        session.setRating(rating) ;
+        sessionEntity.setEndTime(LocalDateTime.now());
+        sessionEntity.setRating(rating) ;
         sessionMap.remove(sessionId);
 
 
@@ -219,18 +218,18 @@ public class ServerManager {
 
         System.out.println("==== SERVER STATISTICS ====");
 
-        for (Server server : servers) {
-            int servedToday = server.getNumClientsDay();
-            int servedMonth = server.getNumClientsMonth();
-            double avgRating = server.getAverageRating();
+        for (ServerEntity serverEntity : serverEntities) {
+            int servedToday = serverEntity.getNumClientsDay();
+            int servedMonth = serverEntity.getNumClientsMonth();
+            double avgRating = serverEntity.getAverageRating();
 
             totalServedToday += servedToday;
             totalServedMonth += servedMonth;
 
-            totalRating += server.getRatingTotal();
-            totalRatingCount += server.getRatingCount();
+            totalRating += serverEntity.getRatingTotal();
+            totalRatingCount += serverEntity.getRatingCount();
 
-            System.out.println("Server: " + server.getId());
+            System.out.println("Server: " + serverEntity.getId());
             System.out.println(" Clients served today: " + servedToday);
             System.out.println(" Clients served this month: " + servedMonth);
             System.out.println(" Average Rating: " + avgRating);
